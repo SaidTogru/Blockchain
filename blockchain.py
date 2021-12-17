1e6c55e0887a999e22f6f03424461c702e6b6e2f
SaidTogru <said.togru@gmail.com>
2021-12-17 20:28:41 +0100

    format

--- blockchain.py
+++ blockchain.py
@@ -2,35 +2,56 @@ from hashlib import sha256
 
 
 def updatehash(*args):
-    hashing_text="hash"
-    h=sha256()
+    hashing_text = "hash"
+    h = sha256()
     for arg in args:
-        hashing_text+=str(arg)
-    h.update(hashing_text.encode('utf-8'))
+        hashing_text += str(arg)
+    h.update(hashing_text.encode("utf-8"))
     return h.hexdigest()
-    
-class Block():
-    data=None
-    hash=None
-    nonce=0
-    previous_hash="0"*64
-    
-    def __init__(self,data,number=0):
-        self.data=data
-        self.number=number
-        
+
+
+class Block:
+    data = None
+    hash = None
+    nonce = 0
+    previous_hash = "0" * 64
+
+    def __init__(self, data, number=0):
+        self.data = data
+        self.number = number
+
     def hash(self):
         return updatehash(self.previous_hash, self.number, self.data, self.nonce)
-    def __str__(self):
-        return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n"%(self.number,self.hash(),self.previous_hash,self.data,self.nonce))
 
-class Blockchain():
-    pass
+    def __str__(self):
+        return str(
+            "Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n"
+            % (self.number, self.hash(), self.previous_hash, self.data, self.nonce)
+        )
+
+
+class Blockchain:
+    difficulty = 4
+
+    def __init__(self, chain=[]):
+        self.chain = chain
+
+    def add(self, block):
+        self.chain.append(
+            {
+                "hash": block.hash(),
+                "previous": block.previous_hash,
+                "number": block.number,
+                "data": block.data,
+                "nonce": block.nonce,
+            }
+        )
+
 
 def main():
-    block=Block("Genesis",1)
+    block = Block("Genesis", 1)
     print(block)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
