0e197c078465b2f484982f245bf4ba6523e2d338
SaidTogru <said.togru@gmail.com>
2021-12-30 16:15:38 +0100

    update

--- test.py
+++ test.py
@@ -6,9 +6,10 @@ from flask import Flask, jsonify, request
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
-url = "https://httpbin.org/post"  # Set destination URL here
+url = "http://127.0.0.1:5000/api/fetch_transaction"  # Set destination URL here
 post_fields = {"foo": "bar"}  # Set POST fields here
 
 request = Request(url, urlencode(post_fields).encode())
 json = urlopen(request).read().decode()
 print(json)
+print("opfer")
